b2bb2ab45bc88867d24a220b0a0f779f1f4f2736
charlit <lesmarmottons@hotmail.fr>
2019-01-03 07:11:19 +0100

    Propriétés personnalisées

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
-  appareilName = 'Machine à laver';
-  appareilStatus = 'éteint';
+  @Input() appareilStatus: string;
+  @Input() appareilName: string;
   constructor() { }
 
   ngOnInit() {
