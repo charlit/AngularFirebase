65ca8462eacebe0321f0904d255d089fbb6a4992
jussanc <cjussan@cafat.nc>
2019-01-04 15:35:08 +1100

    Faites communiquer vos components

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil',
@@ -8,7 +9,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AppareilComponent implements OnInit {
   @Input() appareilStatus: string;
   @Input() appareilName: string;
-  constructor() { }
+  @Input() index: number;
+
+  constructor(private appareilService: AppareilService) { }
 
   ngOnInit() {
   }
@@ -24,4 +27,11 @@ export class AppareilComponent implements OnInit {
       return 'red';
     }
 }
+  onSwitch() {
+    if(this.appareilStatus === 'allumé') {
+      this.appareilService.switchOffOne(this.index);
+    } else if(this.appareilStatus === 'éteint') {
+      this.appareilService.switchOnOne(this.index);
+    }
+  }
 }
