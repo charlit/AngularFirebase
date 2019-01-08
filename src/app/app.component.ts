0a4d3931f3dfb45851bc6d89f37de038719fd038
jussanc <cjussan@cafat.nc>
2019-01-08 11:45:12 +1100

    status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppareilService } from './services/appareil.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent   {
+export class AppComponent  {
  
 
 }
