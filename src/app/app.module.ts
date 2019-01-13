0e414fc3f5fe2bac2e603cd661c20ac8f0ffb222
jussanc <cjussan@cafat.nc>
2019-01-14 10:24:45 +1100

    guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,13 @@ import { SingleAppareilComponent } from './single-appareil/single-appareil.compo
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
+
+
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
-  { path: 'appareils/:id', component: SingleAppareilComponent },
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
@@ -44,7 +47,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     AppareilService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
