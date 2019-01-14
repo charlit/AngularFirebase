f2001a8682215260a79dd9ac6934ebb7361f86a1
jussanc <cjussan@cafat.nc>
2019-01-14 15:18:57 +1100

    observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Component, OnInit } from '@angular/core';
+//import { Observable } from 'rxjs';
+//import 'rxjs/add/Observable/interval';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
- 
+export class AppComponent implements OnInit {
+  secondes: number;
 
+  ngOnInit() {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
 }
